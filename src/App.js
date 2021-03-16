bd64f0621544450bb5fc782605904dc50f4e7f68
Joe Zanchetti <joey22zanch@gmail.com>
2021-03-15 23:22:24 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useRef } from "react";
+//styles
+import "./styles/app.scss";
+//components
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import Nav from "./components/Nav";
+//utility
+import data from "./util";
+
+function App() {
+  //ref
+  const audioRef = useRef(null);
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
+  const [libraryStatus, setLibraryStatus] = useState(false);
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration });
+  };
+  return (
+    <div className="App">
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        setSongs={setSongs}
+      />
+      <Library
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        audioRef={audioRef}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus={libraryStatus}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LibrarySong.js
@@ -0,0 +1,56 @@
+import { faNewspaper } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const LibrarySong = ({
+  song,
+  songs,
+  setCurrentSong,
+  id,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelectHandler = () => {
+    const selectedSong = songs.filter((state) => state.id === id);
+    //console.log(selectedSong);
+    setCurrentSong(selectedSong[0]);
+    //active state
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    //check song
+    if (isPlaying) {
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        });
+      }
+    }
+  };
+  return (
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
+      <img alt={song.name} src={song.cover}></img>
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
